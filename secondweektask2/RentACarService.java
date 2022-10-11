814107502fc4f97ae548377b782dd754169c8528
ozgekatirci <ozgeozge1666@gmail.com>
2022-10-11 19:00:06 +0300

    updated

--- secondweektask2/RentACarService.java
+++ secondweektask2/RentACarService.java
@@ -1,6 +1,6 @@
 package firstweektask1.secondweektask2;
 
-public interface RentACar {
+public interface RentACarService {
    void dailyRent(Customer c,int period);
    void monthlyRent(Customer c, int period);
 }
